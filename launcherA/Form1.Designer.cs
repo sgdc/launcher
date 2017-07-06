0f6a675aba9c8c463e62e217d7f325830424adc6
Adam Gincel <agincel@stevens.edu>
2017-07-05 23:14:43 -0400

    Add project files.

---
+++ launcherA/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace launcherA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblList = new System.Windows.Forms.Label();
+            this.picGif = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGif)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::launcherA.Properties.Resources.SGDC_logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 101);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.Font = new System.Drawing.Font("NK57 Monospace Cd Rg", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(511, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(741, 135);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Game Name";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDesc.Font = new System.Drawing.Font("NK57 Monospace Cd Rg", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesc.Location = new System.Drawing.Point(626, 467);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(496, 184);
+            this.lblDesc.TabIndex = 4;
+            this.lblDesc.Text = "This is a placeholder description.";
+            this.lblDesc.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblList
+            // 
+            this.lblList.Font = new System.Drawing.Font("NK57 Monospace Cd Rg", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblList.Location = new System.Drawing.Point(12, 121);
+            this.lblList.Name = "lblList";
+            this.lblList.Size = new System.Drawing.Size(341, 551);
+            this.lblList.TabIndex = 5;
+            this.lblList.Text = "Game A\r\nGame B\r\nGame C\r\nGame D\r\n> Game E <\r\nGame F\r\nGame G";
+            this.lblList.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // picGif
+            // 
+            this.picGif.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.picGif.Location = new System.Drawing.Point(639, 194);
+            this.picGif.Name = "picGif";
+            this.picGif.Size = new System.Drawing.Size(480, 270);
+            this.picGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picGif.TabIndex = 6;
+            this.picGif.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.picGif);
+            this.Controls.Add(this.lblList);
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "SGDC Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picGif)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblList;
+        private System.Windows.Forms.PictureBox picGif;
+    }
+}
+
---
+++ launcherA/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace launcherA
+{
+
+    public partial class Form1 : Form
+    {
+        List<Game> gamesList;
+        int selected = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            string games = "";
+            foreach (string line in File.ReadLines("games.json"))
+            {
+                games += line;
+            }
+            gamesList = JsonConvert.DeserializeObject<List<Game>>(games);
+            populateSelectedGame();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                MessageBox.Show("You pressed the F1 key");
+                return true;    // indicate that you handled this keystroke
+            }
+            else if (keyData == Keys.Up)
+            {
+                selected -= 1;
+                if (selected < 0)
+                    selected = gamesList.Count - 1;
+                populateSelectedGame();
+                return true;
+            }
+            else if (keyData == Keys.Down)
+            {
+                selected += 1;
+                if (selected >= gamesList.Count)
+                    selected = 0;
+                populateSelectedGame();
+                return true;
+            }
+            else if (keyData == Keys.Enter || keyData == Keys.Space)
+            {
+                try
+                {
+                    Process.Start(gamesList[selected].path);
+                } catch (Exception)
+                {
+                    selected = 0;
+                    populateSelectedGame();
+                }
+            }
+
+            // Call the base class
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        void populateSelectedGame ()
+        {
+            string listText = "";
+            for (int i = 0; i < gamesList.Count; i++)
+            {
+                if (i == selected)
+                {
+                    listText += "> " + gamesList[i].name + " <\n";
+                } else
+                {
+                    listText += gamesList[i].name + "\n";
+                }
+            }
+            ((Label)GetControlByName("lblList")).Text = listText;
+            ((Label)GetControlByName("lblTitle")).Text = gamesList[selected].name;
+            ((Label)GetControlByName("lblDesc")).Text = gamesList[selected].description;
+            ((PictureBox)GetControlByName("picGif")).Image = Image.FromFile(gamesList[selected].gifPath);
+            ((PictureBox)GetControlByName("picGif")).Refresh();
+        }
+
+        Control GetControlByName(string Name)
+        {
+            foreach (Control c in this.Controls)
+                if (c.Name == Name)
+                    return c;
+
+            return null;
+        }
+    }
+
+
+
+
+    public class Game
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public string path { get; set; }
+        public string gifPath { get; set; }
+    }
+}
